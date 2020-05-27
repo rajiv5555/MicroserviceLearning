a4eb71d3fd36fc35238cb89d30edc3c4c4c541ab
Rajiv Ranjan <rajiv.r@zivame.com>
2020-05-27 11:46:40 +0530

    loadbalancer on resttempalte and hystrix uses

--- Movie-Catalog-Services/src/main/java/io/zivame/MovieCatalogServices/MovieCatalogServicesApplication.java
+++ Movie-Catalog-Services/src/main/java/io/zivame/MovieCatalogServices/MovieCatalogServicesApplication.java
@@ -2,11 +2,17 @@ package io.zivame.MovieCatalogServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class MovieCatalogServicesApplication {
 
 	public static void main(String[] args) {
@@ -14,11 +20,13 @@ public class MovieCatalogServicesApplication {
 	}
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
 	
 	@Bean
+	@LoadBalanced
 	public WebClient.Builder getWebclient(){
 		return WebClient.builder();
 	}
--- Movie-Catalog-Services/src/main/java/io/zivame/MovieCatalogServices/controller/MovieController.java
+++ Movie-Catalog-Services/src/main/java/io/zivame/MovieCatalogServices/controller/MovieController.java
@@ -4,18 +4,17 @@
 package io.zivame.MovieCatalogServices.controller;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import io.zivame.MovieCatalogServices.Model.CatalogItem;
 import io.zivame.MovieCatalogServices.Model.Movie;
@@ -36,6 +35,7 @@ public class MovieController {
 	WebClient.Builder webClientBuilder;
 	
 	@GetMapping("/v1/catalog/{userId}")
+	@HystrixCommand(fallbackMethod = "getFallBackCatalog")
 	public List<CatalogItem> getCatalogV1(@PathVariable("userId") String userId){
 
 		List<Rating> ratings= Arrays.asList(
@@ -44,7 +44,7 @@ public class MovieController {
 		);
 		
 		return ratings.stream().map(rating->{
-			Movie movie=restTemplate.getForObject("http://localhost:8082/movie/"+rating.getMovieId(),Movie.class);
+			Movie movie=restTemplate.getForObject("http://MovieInfoService/movie/"+rating.getMovieId(),Movie.class);
 		    return new CatalogItem(movie.getName()," good movie ", rating.getRatingNo());
 		}).collect(Collectors.toList());
 		
@@ -52,6 +52,7 @@ public class MovieController {
 	}
 	
 	@GetMapping("/v1/reactiveCatalog/{userId}")
+	@HystrixCommand(fallbackMethod = "getFallBackCatalog")
 	public List<CatalogItem> getReactiveCatalogV1(@PathVariable("userId") String userId){
 
 		List<Rating> ratings= Arrays.asList(
@@ -63,7 +64,7 @@ public class MovieController {
 			//Movie movie=restTemplate.getForObject("http://localhost:8082/movie/"+rating.getMovieId(),Movie.class);
 		    Movie movie= webClientBuilder.build()
 		                     .get()
-		                     .uri("http://localhost:8082/movie/"+rating.getMovieId())
+		                     .uri("http://MovieInfoService/movie/"+rating.getMovieId())
 		                     .retrieve()
 		                     .bodyToMono(Movie.class)
 		                     .block();
@@ -74,6 +75,7 @@ public class MovieController {
 	}
 	
 	@GetMapping("/v2/catalog/{userId}")
+	@HystrixCommand(fallbackMethod = "getFallBackCatalog")
 	public List<CatalogItem> getCatalogV2(@PathVariable("userId") String userId){
 
 		/*
@@ -81,10 +83,10 @@ public class MovieController {
 		 * Rating("456", 5+"") );
 		 */
 		
-		UserRating ratings=restTemplate.getForObject("http://localhost:8083/ratings/"+userId,UserRating.class);
+		UserRating ratings=restTemplate.getForObject("http://MovieRatingService/ratings/"+userId,UserRating.class);
 		
 		return ratings.getUserRatings().stream().map(rating->{
-			Movie movie=restTemplate.getForObject("http://localhost:8082/movie/"+rating.getMovieId(),Movie.class);
+			Movie movie=restTemplate.getForObject("http://MovieInfoService/movie/"+rating.getMovieId(),Movie.class);
 		    return new CatalogItem(movie.getName()," good movie ", rating.getRatingNo());
 		}).collect(Collectors.toList());
 		
@@ -92,6 +94,7 @@ public class MovieController {
 	}
 	
 	@GetMapping("/v2/reactiveCatalog/{userId}")
+	@HystrixCommand(fallbackMethod = "getFallBackCatalog")
 	public List<CatalogItem> getReactiveCatalogV2(@PathVariable("userId") String userId){
 
 		/*List<Rating> ratings= Arrays.asList(
@@ -100,7 +103,7 @@ public class MovieController {
 		);*/
 		UserRating ratings=webClientBuilder.build()
 				                          .get()
-				                          .uri("http://localhost:8083/ratings/"+userId)
+				                          .uri("http://MovieRatingService/ratings/"+userId)
 				                          .retrieve()
 				                          .bodyToMono(UserRating.class)
 				                          .block();
@@ -109,7 +112,7 @@ public class MovieController {
 			//Movie movie=restTemplate.getForObject("http://localhost:8082/movie/"+rating.getMovieId(),Movie.class);
 		    Movie movie= webClientBuilder.build()
 		                     .get()
-		                     .uri("http://localhost:8082/movie/"+rating.getMovieId())
+		                     .uri("http://MovieInfoService/movie/"+rating.getMovieId())
 		                     .retrieve()
 		                     .bodyToMono(Movie.class)
 		                     .block();
@@ -117,5 +120,9 @@ public class MovieController {
 		}).collect(Collectors.toList());
 		
 		//return Collections.singletonList(new CatalogItem("Titanic", "Test", 4));
+	}
+	
+	public List<CatalogItem> getFallBackCatalog(@PathVariable("userId") String userId){
+	 return Arrays.asList(new CatalogItem("Dummy","Test","0"));
 	}
 }
